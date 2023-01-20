796c120cfa69437023d4e081e0e69af3f62d80f3
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2023-01-20 08:57:14 +0800

    mobile ads fix

--- components/GoogleAds.jsx
+++ components/GoogleAds.jsx
@@ -15,41 +15,44 @@ const GoogleAds = ({ads}) => {
   if (!ads) {
     return (<>
       <ins className="adsbygoogle"
-          style={{display:"block", margin:"auto"}}
-          data-ad-client="ca-pub-4841621324421656"
-          data-ad-slot="6509252855"
-          data-ad-format="auto"
-          data-full-width-responsive="true">
+        style={{display:"block", margin:"auto"}}
+        data-ad-client="ca-pub-4841621324421656"
+        data-ad-slot="6509252855"
+        data-ad-format="auto"
+        data-full-width-responsive="true">
       </ins>
     </>
     )
   } else if (ads == "article") {  
     return (
       <ins className="adsbygoogle"
-          style={{display:"block", textAlign:"center"}}
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="ca-pub-4841621324421656"
-          data-ad-slot="8361661008">
+        style={{display:"block", textAlign:"center"}}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-4841621324421656"
+        data-ad-slot="8361661008">
       </ins>
     )
   } else if (ads == "items") {  
     return (
       <ins className="adsbygoogle"
-          style={{display:"block", textAlign:"center"}}
-          data-ad-format="autorelaxed"
-          data-ad-client="ca-pub-4841621324421656"
-          data-ad-slot="9483170983">
+        style={{display:"block", textAlign:"center"}}
+        data-ad-format="autorelaxed"
+        data-ad-client="ca-pub-4841621324421656"
+        data-matched-content-rows-num="4"
+        data-matched-content-columns-num="1"
+        data-matched-content-ui-type="image_stacked"
+        data-ad-slot="9483170983">
       </ins>
     )
   } else {  
   return (
     <ins className="adsbygoogle"
-        style={{display:"block", height:`${(ads == "infeed") ? "11vh" : `${(ads == "top") ? "15vh" : ads}`}`}}
-        data-ad-format="fluid"
-        data-ad-layout-key="-fb+5w+4e-db+86"
-        data-ad-client="ca-pub-4841621324421656"
-        data-ad-slot="4565886222">
+      style={{display:"block", height:`${(ads == "infeed") ? "11vh" : `${(ads == "top") ? "15vh" : ads}`}`}}
+      data-ad-format="fluid"
+      data-ad-layout-key="-fb+5w+4e-db+86"
+      data-ad-client="ca-pub-4841621324421656"
+      data-ad-slot="4565886222">
     </ins>
   )
 }
