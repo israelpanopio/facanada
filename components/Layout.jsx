620b35677232eee43e04bc3e8ba436211e858e42
Israel Garcia Panopio <75904292+israelpanopio@users.noreply.github.com>
2023-02-11 18:35:22 +0800

    revoult

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -26,7 +26,7 @@ const Layout = ({ children }) => {
     <Head>
       <title>Filipinos Aspirants to Canada</title>
       <meta name="Filipinos Aspirants to Canada" content="by Israel Panopio" />
-      <meta name='ir-site-verification-token' value='438025658'></meta>
+      <meta name='ir-site-verification-token' value='443430322' />
       <link rel="icon" href="/favicon.ico" />
       <link href="https://fonts.googleapis.com/css2?family=Cookie&family=Noto+Sans&display=swap" rel="stylesheet" />
       <meta property="fb:app_id" content="366180864923538" />
